9ef3dfb2ff756863d7a4e2a9bf4694a9b9913bef
Yashwants19 <Yashwantsingh.sngh@gmail.com>
2019-02-16 06:30:09 +0530

    Update print_pyx.cpp

--- src/mlpack/bindings/python/print_pyx.cpp
+++ src/mlpack/bindings/python/print_pyx.cpp
@@ -87,7 +87,7 @@ void PrintPYX(const ProgramDoc& programInfo,
   cout << "cimport numpy as np" << endl;
   cout << endl;
   cout << "from libcpp.string cimport string" << endl;
-  cout << "from libcpp cimport bool as bl" << endl;
+  cout << "from libcpp cimport bool as cbool" << endl;
   cout << "from libcpp.vector cimport vector" << endl;
   cout << endl;
   cout << "from cython.operator import dereference" << endl;
@@ -188,11 +188,11 @@ void PrintPYX(const ProgramDoc& programInfo,
   // Determine whether or not we need to copy parameters.
   cout << "  if isinstance(copy_all_inputs, bool):" << endl;
   cout << "    if copy_all_inputs:" << endl;
-  cout << "      SetParam[bl](<const string> 'copy_all_inputs', "
+  cout << "      SetParam[cbool](<const string> 'copy_all_inputs', "
       << "copy_all_inputs)" << endl;
   cout << "      CLI.SetPassed(<const string> 'copy_all_inputs')" << endl;
   cout << "  else:" << endl;
-  cout << "    raise TypeError(" <<"\"copy_all_inputs must have type 'bool'!\""
+  cout << "    raise TypeError(" <<"\"'copy_all_inputs\' must have type 'bool'!\""
       << ")" << endl;
 
   // Do any input processing.
